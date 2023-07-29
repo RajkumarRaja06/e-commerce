2e91a9950efd104096f3b41066ba1a45d1561b64
rajkumarrraja <110884663+rajkumarrraja@users.noreply.github.com>
2023-07-29 15:49:35 +0530

    added footer component and footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/footer.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer'>
+      <div className='footer_menu_link'>
+        <Link to='shop'>Shop</Link>
+        <Link to='contact'>Contacts us</Link>
+      </div>
+      <p className='footer_para'>
+        ©2023 All Rights Reserverd. This template is made with by{' '}
+        <span>
+          <Link to='https://eloquent-pie-3edde5.netlify.app/' target='_blank'>
+            RajKumarRaja
+          </Link>
+        </span>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
